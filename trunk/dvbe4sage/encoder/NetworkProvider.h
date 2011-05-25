873d6032da023f8c03b35879bccdb2406da0f723
asskick <asskick@237a9cd0-f380-11de-8b01-fd14ac940ccb>
2011-05-25 15:44:27 +0000

    Channel mappings now saved/loaded on the services.cache Added a column on the services.csv for Bouquet ChNo

--- trunk/dvbe4sage/encoder/NetworkProvider.h
+++ trunk/dvbe4sage/encoder/NetworkProvider.h
@@ -51,6 +51,7 @@ public:
 	NetworkProvider() : m_DefaultONID(0)										{}
 	// Common query methods
 	bool getServiceName(UINT32 sid, LPTSTR output, int outputLength) const;
+	USHORT getChannelForSid(UINT32 usid) const;
 	bool getSidForChannel(USHORT channel, UINT32& sid) const;
 	bool getTransponderForSid(UINT32 sid, Transponder& transponder) const;
 	bool getOnidForSid(USHORT sid, USHORT& onid) const;
